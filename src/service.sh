ff588f261725cef76af7046ae87c4da23f3eae25
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2026-02-28 10:39:43 -0800

    fix: properly handle non existing svc

--- src/service.sh
+++ src/service.sh
@@ -3,28 +3,32 @@
 #
 function serviceStatus () {
 
-    # first of all ceck if unit exists!
-    if ! systemctl status "$1" >/dev/null 2>&1; then
-        echo -e -n "$ColorReset""➜ [$Bold$Blue $1$ColorReset$Normal $Yellow⁇$ColorReset$Dim not found ] "
+    local UNIT="$1"
+    [[ "$UNIT" != *.service ]] && UNIT="$UNIT.service"
+
+    # Check if unit actually exists
+    if ! systemctl cat "$UNIT" >/dev/null 2>&1; then
+        print "$ColorReset""➜ [$Bold$Blue $1$ColorReset$Normal $Yellow⁇$ColorReset$Dim not found$ColorReset] "
         return
     fi
 
-    local IS_ACTIVE=$(systemctl is-active "$1")
-    local IS_UP=$(systemctl show -p SubState --value "$1")
-    if [ "$IS_ACTIVE" = "active" ] ; then
-    echo -e -n "$ColorReset""➜ [$Bold$Blue $1$ColorReset$Normal $Green✓$ColorReset$Dim active "
+    local IS_ACTIVE=$(systemctl is-active "$UNIT")
 
-        case $IS_UP in
-            "running") echo -e -n "$Green▲$ColorReset$Dim up$Normal$ColorReset ]" ;;
-            "dead") echo -e -n "$Red▼$ColorReset$Dim down$ColorReset ]" ;;
-            *) echo -e -n "Error loading service" ;;
+    local IS_UP=$(systemctl show -p SubState --value "$UNIT")
+
+    if [[ "$IS_ACTIVE" == "active" ]]; then
+        print "$ColorReset➜ [$Bold$Blue $1$ColorReset$Normal $Green✓$ColorReset$Dim active "
+
+        case "$IS_UP" in
+            running) print "$Green▲$ColorReset$Dim up$Normal$ColorReset ]" ;;
+            dead)    print "$Red▼$ColorReset$Dim down$ColorReset ]" ;;
+            *)       print "$IS_UP ]" ;;
         esac
 
-    elif [ "$IS_ACTIVE" = "inactive" ] ; then
-        echo -e -n "$ColorReset""➜ [$Bold$Blue $1$ColorReset$Normal $Red ✗ $ColorReset inactive ] "
+    elif [[ "$IS_ACTIVE" == "inactive" ]]; then
+        print "$ColorReset➜ [$Bold$Blue $1$ColorReset$Normal $Red✗$ColorReset inactive ]"
 
     else
-        # might not need this one given the status check above
-        echo -e -n "$ColorReset""➜ [$Bold$Blue $1$ColorReset$Normal $Yellow ● $ColorReset no status! ] "
+        print "$ColorReset➜ [$Bold$Blue $1$ColorReset$Normal $Yellow●$ColorReset $IS_ACTIVE ] "
     fi
 }
